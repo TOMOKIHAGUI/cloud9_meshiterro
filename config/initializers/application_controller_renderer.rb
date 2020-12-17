dc072155882ef76645f83ea41cbabf4a247cdab8
TOMOKIHAGUI <higosozan18@gmail.com>
2020-12-17 05:21:31 +0000

    [5-4]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '173585d771be29538b2ddf6d755aa3bd49484939ccade349ad53763302f29e67e0f32e951cfd811045f5ced85e1887331ad3bb4a5c1f18f3a3309f971472ffb0'
